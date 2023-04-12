062a1d711424e3f14ffa41ce1510df1e612adaee
miguelbizzi <miguel.bizzi@aglets.com.br>
2023-04-11 21:41:43 -0300

    Adicionando o hookform na tela de singIn

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,26 +1,44 @@
 import { useNavigation } from "@react-navigation/native";
+import { Controller, useForm } from "react-hook-form";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 
-import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 
-import LogoSvg from '@assets/logo.svg';
-import BackgroundImg from '@assets/background.png';
+import LogoSvg from "@assets/logo.svg";
+import BackgroundImg from "@assets/background.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
-export function SignIn() {
+interface FormData {
+  email: string;
+  password: string;
+}
 
+export function SignIn() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
   function handleNewAccount() {
-    navigation.navigate('signUp');
+    navigation.navigate("signUp");
+  }
+
+  function handleSignIn({ email, password }: FormData) {
+    console.log(email, password);
   }
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-        <VStack flex={1} px={10} pb={16}>
-        <Image 
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} px={10} pb={16}>
+        <Image
           source={BackgroundImg}
           defaultSource={BackgroundImg}
           alt="Pessoas treinando"
@@ -41,18 +59,36 @@ export function SignIn() {
             Acesse a conta
           </Heading>
 
-          <Input 
-            placeholder="E-mail" 
-            keyboardType="email-address"
-            autoCapitalize="none"
-
+          <Controller
+            control={control}
+            name="email"
+            rules={{ required: "Informe o e-mail" }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+                autoCapitalize="none"
+              />
+            )}
           />
-          <Input 
-            placeholder="Senha" 
-            secureTextEntry
+
+          <Controller
+            control={control}
+            name="password"
+            rules={{ required: "Informe a senha" }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+              />
+            )}
           />
 
-          <Button title="Acessar" />
+          <Button title="Acessar" onPress={handleSubmit(handleSignIn)} />
         </Center>
 
         <Center mt={24}>
@@ -60,8 +96,8 @@ export function SignIn() {
             Ainda não tem acesso?
           </Text>
 
-          <Button 
-            title="Criar Conta" 
+          <Button
+            title="Criar Conta"
             variant="outline"
             onPress={handleNewAccount}
           />
