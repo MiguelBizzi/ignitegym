4efb94af1f79f0c1ef8c1a57d2966cca2ea59082
miguelbizzi <miguel.bizzi@aglets.com.br>
2023-04-14 21:55:54 -0300

    Consumindo api para setar exercicios dinamicamente e exercicios details

--- mobile/src/screens/Exercise.tsx
+++ mobile/src/screens/Exercise.tsx
@@ -1,85 +1,162 @@
-import { TouchableOpacity } from 'react-native';
-import { Box, Heading, HStack, Icon, Image, Text, VStack } from 'native-base';
-import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from "react-native";
+import {
+  Box,
+  Heading,
+  HStack,
+  Icon,
+  Image,
+  ScrollView,
+  Skeleton,
+  Text,
+  useToast,
+  VStack,
+} from "native-base";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
-import { AppNavigatorRoutesProps } from '@routes/app.routes';
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
-import BodySvg from '@assets/body.svg';
-import SeriesSvg from '@assets/series.svg';
-import RepetitionsSvg from '@assets/repetitions.svg';
-import { Button } from '@components/Button';
+import BodySvg from "@assets/body.svg";
+import SeriesSvg from "@assets/series.svg";
+import RepetitionsSvg from "@assets/repetitions.svg";
+import { Button } from "@components/Button";
+import { AppError } from "@utils/AppError";
+import { api } from "@services/api";
+import { useEffect, useState } from "react";
+import { IExercise } from "@dtos/IExercise";
+import { Loading } from "@components/Loading";
+
+interface RouteParamsProps {
+  exerciseId: string;
+}
 
 export function Exercise() {
+  const [exercise, setExercise] = useState<IExercise>({} as IExercise);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const navigation = useNavigation<AppNavigatorRoutesProps>();
+  const route = useRoute();
+  const toast = useToast();
+
+  const { exerciseId } = route.params as RouteParamsProps;
 
   function handleGoBack() {
     navigation.goBack();
   }
 
+  async function fetchExerciseDetails() {
+    try {
+      setIsLoading(true);
+      const response = await api.get(`/exercises/${exerciseId}`);
+
+      setExercise(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError
+        ? error.message
+        : "Não foi possível carregar os detalhes do exercício";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchExerciseDetails();
+  }, [exerciseId]);
+
   return (
     <VStack flex={1}>
       <VStack px={8} bg="gray.600" pt={12}>
         <TouchableOpacity onPress={handleGoBack}>
-          <Icon 
-            as={Feather}
-            name="arrow-left"
-            color="green.500"
-            size={6}
-          />
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
         </TouchableOpacity>
 
-        <HStack justifyContent="space-between" mt={4} mb={8} alignItems="center">
-          <Heading color="gray.100" fontSize="lg"  flexShrink={1} fontFamily="heading">
-            Puxada frontal
-          </Heading>
+        <HStack
+          justifyContent="space-between"
+          mt={4}
+          mb={8}
+          alignItems="center"
+        >
+          {isLoading ? (
+            <Skeleton w={20} h={4} rounded={"lg"} />
+          ) : (
+            <Heading
+              color="gray.100"
+              fontSize="lg"
+              flexShrink={1}
+              fontFamily="heading"
+            >
+              {exercise.name}
+            </Heading>
+          )}
 
           <HStack alignItems="center">
             <BodySvg />
-
-            <Text color="gray.200" ml={1} textTransform="capitalize">
-              Costas
-            </Text>
+            {isLoading ? (
+              <Skeleton.Text w={10} lines={1} isLoaded={!isLoading} />
+            ) : (
+              <Text color="gray.200" ml={1} textTransform="capitalize">
+                {exercise.group}
+              </Text>
+            )}
           </HStack>
         </HStack>
       </VStack>
 
-      <VStack p={8}>
-        <Image
-          w="full"
-          h={80}
-          source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
-          alt="Nome do exercício"
-          mb={3}
-          resizeMode="cover"
-          rounded="lg"
-        />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <ScrollView>
+          <VStack p={8}>
+            <Box rounded="lg" mb={3} overflow="hidden">
+              <Image
+                w="full"
+                h={80}
+                source={{
+                  uri: `${api.defaults.baseURL}/exercise/demo/${exercise.demo}`,
+                }}
+                alt="Nome do exercício"
+                resizeMode="cover"
+                rounded="lg"
+              />
+            </Box>
 
-        <Box bg="gray.600" rounded="md" pb={4} px={4}>
-          <HStack alignItems="center" justifyContent="space-around" mb={6} mt={5}>
-            <HStack>
-              <SeriesSvg />
+            <Box bg="gray.600" rounded="md" pb={4} px={4}>
+              <HStack
+                alignItems="center"
+                justifyContent="space-around"
+                mb={6}
+                mt={5}
+              >
+                <HStack>
+                  <SeriesSvg />
 
-              <Text color="gray.200" ml="2">
-                3 séries
-              </Text>
-            </HStack>
+                  <Text color="gray.200" ml="2">
+                    {exercise.series} séries
+                  </Text>
+                </HStack>
 
-            <HStack>
-              <RepetitionsSvg />
-              
-              <Text color="gray.200" ml="2">
-                12 repetições
-              </Text>
-            </HStack>
-          </HStack>
+                <HStack>
+                  <RepetitionsSvg />
 
-          <Button 
-            title="Marcar como realizado"
-          />
-        </Box>
-      </VStack>
+                  <Text color="gray.200" ml="2">
+                    {exercise.repetitions} repetições
+                  </Text>
+                </HStack>
+              </HStack>
+
+              <Button title="Marcar como realizado" />
+            </Box>
+          </VStack>
+        </ScrollView>
+      )}
     </VStack>
   );
 }
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,32 +1,88 @@
-import { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
-import { FlatList, Heading, HStack, Text, VStack } from "native-base";
+import { useCallback, useEffect, useState } from "react";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { FlatList, Heading, HStack, Text, useToast, VStack } from "native-base";
 
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 import { ExerciseCard } from "@components/ExerciseCard";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import { AppError } from "@utils/AppError";
+import { api } from "@services/api";
+import { IExercise } from "@dtos/IExercise";
+import { Loading } from "@components/Loading";
 
 export function Home() {
-  const [groups, setGroups] = useState([
-    "Costas",
-    "Bíceps",
-    "Tríceps",
-    "ombro",
-  ]);
-  const [exercises, setExercises] = useState([
-    "Puxada frontal",
-    "Remada curvada",
-    "Remada unilateral",
-    "Levantamento terras",
-  ]);
-  const [groupSelected, setGroupSelected] = useState("Costas");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [groups, setGroups] = useState<string[]>([]);
+  const [exercises, setExercises] = useState<IExercise[]>([] as IExercise[]);
+  const [groupSelected, setGroupSelected] = useState("antebraço");
 
   const navigation = useNavigation<AppNavigatorRoutesProps>();
+  const toast = useToast();
 
-  function handleOpenExerciseDetails() {
-    navigation.navigate("exercise");
+  function handleOpenExerciseDetails(exerciseId: string) {
+    navigation.navigate("exercise", {
+      exerciseId,
+    });
   }
+
+  async function fetchGroups() {
+    try {
+      setIsLoading(true);
+
+      const response = await api.get("/groups");
+
+      setGroups(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError
+        ? error.message
+        : "Não foi possível carregar os grupos musculares";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function fetchExercisesByGroup() {
+    try {
+      setIsLoading(true);
+
+      const response = await api.get(`/exercises/bygroup/${groupSelected}`);
+
+      setExercises(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError
+        ? error.message
+        : "Não foi possível carregar os exercícios";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchGroups();
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchExercisesByGroup();
+    }, [groupSelected])
+  );
 
   return (
     <VStack>
@@ -53,30 +109,37 @@ export function Home() {
         )}
       />
 
-      <VStack px={8}>
-        <HStack justifyContent="space-between" mb={5}>
-          <Heading color="gray.200" fontSize="md" fontFamily="heading">
-            Exercícios
-          </Heading>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <VStack px={8}>
+          <HStack justifyContent="space-between" mb={5}>
+            <Heading color="gray.200" fontSize="md" fontFamily="heading">
+              Exercícios
+            </Heading>
 
-          <Text color="gray.200" fontSize="sm">
-            {exercises.length}
-          </Text>
-        </HStack>
+            <Text color="gray.200" fontSize="sm">
+              {exercises.length}
+            </Text>
+          </HStack>
 
-        <FlatList
-          data={exercises}
-          keyExtractor={(item) => item}
-          renderItem={({ item }) => (
-            <ExerciseCard onPress={handleOpenExerciseDetails} />
-          )}
-          showsVerticalScrollIndicator={false}
-          _contentContainerStyle={{
-            paddingBottom: 5,
-          }}
-          h={"xs"}
-        />
-      </VStack>
+          <FlatList
+            data={exercises}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <ExerciseCard
+                onPress={() => handleOpenExerciseDetails(item.id)}
+                data={item}
+              />
+            )}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{
+              paddingBottom: 5,
+            }}
+            h={"xs"}
+          />
+        </VStack>
+      )}
     </VStack>
   );
 }
